221c8d23a9bd49df3dfe8d41c4d819b40510b0c0
Daris <mvrcdaris@gmail.com>
2024-02-15 18:25:52 +0100

    Created UI For Chat

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,13 +1,66 @@
 import "./Home.css"
+import { IoMdSend } from "react-icons/io";
 
 export const Home = () => {
     return (
         <div className="chatter">
             <div className="sidebar">
-                <h1>Chatter</h1>
+                <div className="header">
+                    <h1>Chatter</h1>
+                </div>
+                <div className="users">
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                    <div className="user">
+                        <p>Daris</p>
+                    </div>
+                </div>
+                <div className="footer">
+                    <div className="my-profile">
+                        <p>Moje Ime</p>
+                    </div>
+                </div>
             </div>
             <div className="chat">
-                <h1>Welcome,admin</h1>
+                <div className="no-chat">
+                    <h1>Select a Contact and start Chatting</h1>
+                </div>
+                <div className="user-chat">
+                    <div className="user-profile">
+                        <h1>Daris</h1>
+                    </div>
+                    <div className="messages">
+                        <div className="sender-message">
+                            <p>Hi</p>
+                        </div>
+                        <div className="reciever-message">
+                            <p>Hi</p>
+                        </div>
+                    </div>
+                    <div className="send-message">
+                        <input type="text" placeholder="type some message here"/>
+                        <button><IoMdSend/></button>
+                    </div>
+                </div>
             </div>
         </div>
     )
